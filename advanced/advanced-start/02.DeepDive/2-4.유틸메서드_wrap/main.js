3ca8231c1ba3b1028e4f70e57febe321f68320a6
신민경 <80gaeng@hanmail.net>
2026-05-15 08:41:41 +0900

    공부중

--- advanced/advanced-start/02.DeepDive/2-4.유틸메서드_wrap/main.js
+++ advanced/advanced-start/02.DeepDive/2-4.유틸메서드_wrap/main.js
@@ -1,8 +1,23 @@
+gsap.to('.stage', { autoAlpha: 1 });
 
+const split = new SplitText('h1', { type: 'chars' });
 
+const tl = gsap.timeline();
 
-gsap.to('.stage',{autoAlpha:1})
+tl.from(split.chars, {
+  y: gsap.utils.wrap([-100, 100]),
+  opacity: 0,
+  stagger: {
+    each: 0.02,
+    from: 'random',
+  },
+}).to(split.chars, {
+  x: 10,
+  y: gsap.utils.wrap([0, 200, 0, 200, 200, 0]),
+  color: gsap.utils.wrap(['red', 'blue']),
+  stagger: {
+    each: 0.02,
+  },
+});
 
-
-
-GSDevTools.create()
+GSDevTools.create();
