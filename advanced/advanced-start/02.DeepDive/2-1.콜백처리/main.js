b5a80420fde3345365ec1c94a3d1740a2c349886
신민경 <80gaeng@hanmail.net>
2026-05-14 13:54:59 +0900

    공부중

--- advanced/advanced-start/02.DeepDive/2-1.콜백처리/main.js
+++ advanced/advanced-start/02.DeepDive/2-1.콜백처리/main.js
@@ -1,38 +1,36 @@
-const $ = node => document.querySelector(node);
+const $ = (node) => document.querySelector(node);
 
-
-//  callback -> 나중에 일어나는 일 
+//  callback -> 나중에 일어나는 일
 
 // onComplete
 // onUpdate
 // onStart
 // onRepeat
 
+const h1 = $('h1');
 
+const tl = gsap.timeline();
 
+gsap.to('.orange', {
+  duration: 2.5,
+  y: 100,
+  // repeat: 5,
+  onComplete: complete,
+  onCompleteParams: ['오렌지'],
+  onUpdate() {
+    h1.textContent = `애니메이션 재생 중`;
+  },
+  onStart() {
+    console.log('시작');
+  },
+  onRepeat() {
+    console.log('반복');
+  },
+});
 
+function complete(color) {
+  // console.log(color);
+  h1.textContent = `${color} 애니메이션 재생 끝`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  gsap.to(this.targets()[0], { rotation: 360 });
+}
--- advanced/advanced-start/02.DeepDive/2-2.타임라인 비주얼라이저/main.js
+++ advanced/advanced-start/02.DeepDive/2-2.타임라인 비주얼라이저/main.js
@@ -1,32 +1,60 @@
-const $ = node => document.querySelector(node);
+const $ = (node) => document.querySelector(node);
 
+gsap.to('svg', { autoAlpha: 1 });
 
+const pixelPerSecond = 200;
 
-gsap.to('svg',{autoAlpha:1})
+const animation = gsap.timeline();
 
+animation
+  .to('#star', { duration: 1, x: 1150 })
+  .to('#circle', { duration: 2, x: 1150 })
+  .to('#square', { duration: 1, x: 1150 });
 
+const children = animation.getChildren();
+const childrenList = children.length;
 
+children.forEach((item, index) => {
+  gsap.set('#tween' + index, { x: item.startTime() * pixelPerSecond });
+  gsap.set('#rect' + index, { width: item.duration() * pixelPerSecond });
+});
 
+/* for (let i = 0; i < childrenList; i++) {
+  gsap.set('#tween' + i, { x: children[i].startTime() * pixelPerSecond });
+  gsap.set('#rect' + i, { width: children[i].duration() * pixelPerSecond });
+  // console.log(children[i].startTime());
+} */
 
+const time = $('#time');
+const maxX = animation.duration() * pixelPerSecond;
 
+function handleMoveHead() {
+  // console.log(animation.time());
+  time.textContent = animation.time().toFixed(1);
+  gsap.set('#playhead', { x: animation.progress() * maxX });
+}
 
+animation.eventCallback('onUpdate', handleMoveHead);
 
+const dragger = Draggable.create('#playhead', {
+  type: 'x',
+  cursor: 'pointer',
+  trigger: '#timeline',
+  bounds: { minX: 0, maxX: maxX },
+  onDrag() {
+    animation.pause();
+    animation.progress(this.x / maxX);
+  },
+});
 
+$('#play').addEventListener('click', () => {
+  animation.play();
+});
 
+$('#pause').addEventListener('click', () => {
+  animation.pause();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('#reverse').addEventListener('click', () => {
+  animation.reverse();
+});
